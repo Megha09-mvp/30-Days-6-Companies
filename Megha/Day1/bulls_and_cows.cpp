d881197f608cc451d58de6b950f81d0bddb2c968
Megha09 <85820039+Megha09-mvp@users.noreply.github.com>
2023-01-09 21:20:12 +0530

    Add files via upload

--- Megha/Day1/bulls_and_cows.cpp
+++ Megha/Day1/bulls_and_cows.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int t=secret.size();
+        int b=0;
+        int c=0;
+        for(int i=0;i<t;i++)
+        {
+            if(secret[i]==guess[i])
+            b++;
+        }
+        map<char,int> mp;
+        for(auto it: secret)
+        {
+            mp[it]++;
+        }
+        for(int j=0;j<t;j++)
+        {
+            if(mp[guess[j]]>0)
+            {
+                c++;
+                mp[guess[j]]--;
+            }
+        }
+        string ans= to_string(b)+"A"+ to_string(c-b)+"B";
+        return ans;
+    }
+};
